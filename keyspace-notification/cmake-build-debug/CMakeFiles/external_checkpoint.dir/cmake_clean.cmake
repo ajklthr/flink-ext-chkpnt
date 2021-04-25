aca8af68be1d8a4277db15e00deebbb8f0d4d7b3
Rahul Balakrishnan <rahulb@DESKTOP-HU4TIM1.localdomain>
2021-04-25 15:54:51 -0700

    Adding first version of redis keyspace notification

---
+++ keyspace-notification/cmake-build-debug/CMakeFiles/external_checkpoint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/external_checkpoint.dir/main.cpp.o"
+  "external_checkpoint"
+  "external_checkpoint.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/external_checkpoint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
