693fe1b9a7f5847d1f5978eb0a89ce5be8e32b5e
Rahul Balakrishnan <rahulb@Rahuls-MacBook-Pro.local>
2021-04-28 21:45:53 -0700

    Adding zookeeper url in server properties

--- kafka/scripts/kafka-deployment-script.sh
+++ kafka/scripts/kafka-deployment-script.sh
@@ -1,10 +1,11 @@
-mkdir /home/shared/kafka & cd /home/shared/kafka
-curl https://downloads.apache.org/kafka/2.8.0/kafka-2.8.0-src.tgz -o kafka.tgz
+mkdir /home/shared/kafka
+cd /home/shared/kafka
+curl https://downloads.apache.org/kafka/2.8.0/kafka_2.12-2.8.0.tgz -o kafka.tgz
 tar -xvzf kafka.tgz --strip 1
 rm kafka.tgz
 curl -s -H "Accept:application/vnd.github.v3.raw" https://api.github.com/repos/rbala19/flink-ext-chkpnt/contents/kafka/scripts/server.properties > config/server.properties
 curl -s -H "Accept:application/vnd.github.v3.raw" https://api.github.com/repos/rbala19/flink-ext-chkpnt/contents/kafka/scripts/zookeeper.properties > config/zookeeper.properties
 cd /etc/systemd/system/
 curl -s -H "Accept:application/vnd.github.v3.raw" https://api.github.com/repos/rbala19/flink-ext-chkpnt/contents/kafka/scripts/kafka.service > kafka.service 
-systemctl enable kafka
-systemctl start kafka
+cd /home/shared/kafka
+echo Setup is almost complete - set a unique broker id within server.properties and run the following command to start kafka: \ kafka/bin/kafka-server-start.sh kafka/config/server.properties
