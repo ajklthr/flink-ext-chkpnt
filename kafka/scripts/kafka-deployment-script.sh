e74b7bad0bd6123bf66510db57dad09cab70fc2f
Rahul Balakrishnan <rahulb@Rahuls-MacBook-Pro.local>
2021-04-28 21:19:37 -0700

    fixing script errors

--- kafka/scripts/kafka-deployment-script.sh
+++ kafka/scripts/kafka-deployment-script.sh
@@ -1,8 +1,7 @@
-mkdir /home/shared/kafka & cd mkdir /home/shared/kafka
+mkdir /home/shared/kafka & cd /home/shared/kafka
 curl https://downloads.apache.org/kafka/2.8.0/kafka-2.8.0-src.tgz -o kafka.tgz
 tar -xvzf kafka.tgz --strip 1
 rm kafka.tgz
-mkdir log
 curl -s -H "Accept:application/vnd.github.v3.raw" https://api.github.com/repos/rbala19/flink-ext-chkpnt/contents/kafka/scripts/server.properties > config/server.properties
 curl -s -H "Accept:application/vnd.github.v3.raw" https://api.github.com/repos/rbala19/flink-ext-chkpnt/contents/kafka/scripts/zookeeper.properties > config/zookeeper.properties
 cd /etc/systemd/system/
